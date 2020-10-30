08c16466727f9ce94f954b53d2633feb68ef16b1
EUGINELETHAL <ochungeugine@gmail.com>
2020-10-30 06:03:35 +0300

    updated travis

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'backend_challenge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'NAME':orders,
+        'USER':lucy,
+        'PASSWORD':orders,
+        'HOST':,
         'PORT': '',
     }
 }

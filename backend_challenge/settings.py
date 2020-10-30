3c6336e9f2d3ebd07e5ee8da0bf33c0b3eb50710
EUGINELETHAL <ochungeugine@gmail.com>
2020-10-30 06:16:50 +0300

    updated travis

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'backend_challenge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'orders',
-        'USER':'lucy',
-        'PASSWORD':'postgres'
-       
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 REST_FRAMEWORK = {

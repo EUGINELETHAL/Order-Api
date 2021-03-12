9939ce4fde28a03020285ae060b8c4a3a2f0649b
EUGINELETHAL <ochungeugine@gmail.com>
2021-03-12 05:28:48 +0300

    used docker

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
-        "HOST":config("DB_HOST"),
+        "HOST":"db",
         "PORT": 5432,
     }
 }
